34bc507a9a37e2176b20b8713183c97a63e1df5d
JedizLaPulga <86472254+JedizLaPulga@users.noreply.github.com>
2026-03-25 13:32:59 +0100

    Add variadic Vector ctor and example usage
    Replace the old Vector constructor with a variadic Args... constructor that allocates capacity, disables default/copy construction, and emplaces elements with a static type check. Initialize internal pointer to null and import malloc/free and core.lifetime::emplace for proper allocation and placement. Remove the extern(C) wrapper and add a short demo in app.d creating Vector!int and Vector!string instances.

--- source/src/vector.d
+++ source/src/vector.d
@@ -1,14 +1,28 @@
 module src.vector;
-extern(C){
-    
+
     struct Vector(T){
-        private T* data = void;
+
+        // private members
+        private T* data = null;
         private size_t length;
         private size_t capacity;
+        import core.stdc.stdlib : malloc, free;
 
-        this(size_t length){
-            this.length = length;
-            this.data = cast(T*)malloc(length * T.sizeof);
+        @disable this();
+        @disable this(this);
+
+        this(Args...)(Args args){
+            this.length = args.length;
+            this.capacity = args.length;
+            this.data = cast(T*)malloc(this.capacity * T.sizeof);
+
+            static foreach (i; 0 .. args.length) {
+            // Check if the argument can actually be assigned to T
+            static assert(is( typeof(args[i]) : T),  "Argument at index "  ~ i.stringof ~  
+            " is not compatible with " ~ T.stringof);
+                import core.lifetime : emplace;
+                emplace(&data[i], args[i]);
+            }
         }
 
 
@@ -22,5 +36,3 @@ extern(C){
     }
 
 
-
-}
