297534b312b7c255137a2d6a596087a5ea661ab9
JedizLaPulga <86472254+JedizLaPulga@users.noreply.github.com>
2026-03-25 13:44:28 +0100

    Refactor Vector internals and add opAssign
    Rename Vector private fields (data, length, capacity) to _data, _length, _capacity and update constructor, emplace usage and destructor to use the new names. Add an opAssign that swaps internal resources to support assignment/move semantics. Also update source/app.d example to assign vec2 = vec instead of creating a separate string vector.

--- source/app.d
+++ source/app.d
@@ -4,7 +4,7 @@ import src.vector;
 void main(){
 
     auto vec = Vector!int(1, 2, 3, 4, 5);
-    auto vec2 = Vector!string("Hello", "World", "D", "Programming", "Language");
+    auto vec2 = vec;
    
 
 }
--- source/src/vector.d
+++ source/src/vector.d
@@ -3,34 +3,39 @@ module src.vector;
     struct Vector(T){
 
         // private members
-        private T* data = null;
-        private size_t length;
-        private size_t capacity;
+        private T* _data = null;
+        private size_t _length;
+        private size_t _capacity;
         import core.stdc.stdlib : malloc, free;
 
         @disable this();
         @disable this(this);
 
         this(Args...)(Args args){
-            this.length = args.length;
-            this.capacity = args.length;
-            this.data = cast(T*)malloc(this.capacity * T.sizeof);
+            this._length = args.length;
+            this._capacity = args.length;
+            this._data = cast(T*)malloc(this._capacity * T.sizeof);
 
             static foreach (i; 0 .. args.length) {
-            
-        
             // Check if the argument can actually be assigned to T
             static assert(is( typeof(args[i]) : T),  "Incompactible type for Vector!T: " 
                 ~typeof(args[i]).stringof);
                 import core.lifetime : emplace;
-                emplace(&data[i], args[i]);
+                emplace(&this._data[i], args[i]);
             }
+        }
+
+        void opAssign(Vector!T other) {
+            import std.algorithm : swap;
+            swap(this._data, other._data);
+            swap(this._length, other._length);
+            swap(this._capacity, other._capacity);
         }
 
 
 
         ~this(){
-            free(data);
+            free(_data);
         }
 
     
